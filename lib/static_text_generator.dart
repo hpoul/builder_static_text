fe8854475caafe5708c8e6501bb19ec12b4a4561
Herbert Poul <herbert@poul.at>
2019-05-13 12:18:30 +0200

    initial commit.

---
+++ lib/static_text_generator.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:build/src/builder/build_step.dart';
+import 'package:source_gen/source_gen.dart';
+
+class StaticTextGenerator extends Generator {
+  StaticTextGenerator(this.content);
+
+  final String content;
+
+  @override
+  FutureOr<String> generate(LibraryReader library, BuildStep buildStep) => content;
+}
+
+Builder staticTextBuilder(BuilderOptions options) => SharedPartBuilder(
+      [
+        StaticTextGenerator(options.config['content'] as String ?? '// modify build.yaml to configure this text.'),
+      ],
+      'static_text',
+    );
